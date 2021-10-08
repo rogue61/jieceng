ca6031712971943e556987c2df0546f596ab33f8
rogue61 <90953676+rogue61@users.noreply.github.com>
2021-10-08 13:00:17 +0800

    Add files via upload

---
+++ 阶层.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int fact(int n)
+{
+	if(n==0)
+	return 1;
+	
+	return  n*fact(n-1);
+	
+ } 
+ int main()
+ {
+ 	int result =fact(5);
+ 	cout<<result<<endl;
+ 	return 0;
+ }
